1ffaeb30ffe0d4ce77287ce2152a91050f98c4e2
Cxlopez <cristian4402@hotmail.com>
2022-11-09 21:26:15 -0500

    useState for animelis, topanime, search

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
+import { useState, useEffect } from "react";
 import Header from "./components/Header";
 
 function App() {
+
+  const [animeList, setAnimeList] = useState([]); 
+  const [topAnime, setTopAnime] = useState([]);
+  const [search, setSearch] = useEffect('');
+
   return (
     <div className="App">
       <Header />
