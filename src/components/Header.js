7efc2d42ff56823a1d8d8b126d44ea7e1d570a17
Cxlopez <cristian4402@hotmail.com>
2022-11-09 21:20:55 -0500

    header css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import '../styles/Header.css';
 
 function Header() {
   return (
@@ -8,4 +9,4 @@ function Header() {
   )
 }
 
-export default Header
+export default Header;
