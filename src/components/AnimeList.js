fa1d10a33e8c26f34d3cc33cfcd30b82f4d8c8bb
Cxlopez <cristian4402@hotmail.com>
2022-11-25 21:03:36 -0500

    add img to AnimeList component

--- src/components/AnimeList.js
+++ src/components/AnimeList.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
-export const AnimeList = () => {
+export default function AnimeList() {
   return (
-    <div>
-      
+    <div className='card'>
+      <img src="https://cdn.myanimelist.net/images/anime/1208/94745l.jpg" alt="animeImg" />
+      <div className='anime-info'>
+        <h4>Full-Metal Alchemist</h4>
+      </div>
     </div>
   )
 }
