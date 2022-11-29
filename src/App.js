9455d627799fd00e5c89751dd008ca805a1c869e
Cxlopez <cristian4402@hotmail.com>
2022-11-28 21:31:01 -0500

    eslint corrections

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-unused-vars */
 import { useState, useEffect } from "react";
 import NavBar from "./components/NavBar";
 import Header from "./components/Header";
