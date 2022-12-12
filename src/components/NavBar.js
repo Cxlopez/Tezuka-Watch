b64bec353bd4ae86678315c2e86f23b6b44d4cd8
Cxlopez <cristian4402@hotmail.com>
2022-12-11 20:02:21 -0500

    use animeList prop

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,13 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
 
-function NavBar() {
+function NavBar(props) {
   return (
     <div>
       <h3>Top anime</h3>
       <nav>
         <a href='#' target="_blank" rel="noreferrer">
-          Attack on titan
+          {props.animeList}
         </a>
         <a href='#' target="_blank" rel="noreferrer">
           One Piece
