519ae49033789ed9d8c0b6f61e4244a4435f010d
Cxlopez <cristian4402@hotmail.com>
2022-12-19 22:02:07 -0500

    map through animeList in Navbar component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,19 +1,31 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
 
-function NavBar(props) {
+function NavBar({ animeList }) {
   return (
-    <div>
-      <h3>Top anime</h3>
-      <nav>
-        <a href='#' target="_blank" rel="noreferrer">
-          {props.animeList}
-        </a>
-        <a href='#' target="_blank" rel="noreferrer">
-          One Piece
-        </a>
-      </nav>
-    </div>
+    <>
+      {
+        animeList ? (
+          animeList.map((anime, index) => {
+            return (
+              <div>
+                <h3>Top anime</h3>
+                <nav>
+                  <a href='#' target="_blank" rel="noreferrer">
+                    {anime.episodes}
+                  </a>
+                  <a href='#' target="_blank" rel="noreferrer">
+                    One Piece
+                  </a>
+                </nav>
+              </div>
+            )
+          })
+        ) : "No Data"
+      }
+    </>
+
+
   )
 }
 
